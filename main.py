77afc0ef008ad735172e8c80211d1fe8b6b47195
shashikantsingh <48221957+shashikant231@users.noreply.github.com>
2021-02-23 10:13:55 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+import csv
+import random
+import datetime as dt
+import smtplib
+
+with open("quotes.txt","r") as new_file:
+    all_quotes = new_file.readlines()
+
+
+
+
+        
+my_email = "trackeriss511@gmail.com"
+password = "domd1234"
+
+now = dt.datetime.now()
+weekday  = now.weekday()
+#if today is monday
+if weekday == 0:
+    x = random.choice(all_quotes)
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(my_email,password)
+        connection.sendmail(
+            from_addr=my_email ,
+            to_addrs= my_email ,
+            msg = "subject:Today Quotes \n\n Good Morning \n{}".format(x)
+         )
